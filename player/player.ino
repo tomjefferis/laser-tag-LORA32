2284211e6ae3811d2a88007d52fe7ebf0be3ddc5
Tom Jefferis <tomjefferis@gmail.com>
2022-10-18 16:55:33 +0100

    setup

--- player/player.ino
+++ player/player.ino
@@ -1,11 +1,43 @@
 #include <IRremote.hpp>
 #include <LoRa.h>
+#include <SPI.h>
+#include <WiFi.h>
 
-
+//LoRa Pins
+#define ss 5
+#define rst 14
+#define dio0 2
+bool connected = False;
+int playerNumber;
 
 void setup() {
   // initilize serial
   Serial.begin(115200);
+  // initilize LoRa and IR
+  LoRa.setPins(ss, rst, dio0);
+  LoRa.begin(866E6);
+  // sync word to only take commands from server
+  LoRa.setSyncWord(0xF3);
+  // connects to server with wifi mac 
+
+  while (!connected)
+  {
+    //if timer interval has been reached
+    LoRa.beginPacket();
+    LoRa.print(WiFi.macAddress());
+    LoRa.endPacket();
+
+    
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+      // read packet
+      while (LoRa.available()) {
+      playerNumber = LoRa.read();
+      connected = True;
+      }
+    }
+  }
+  
   
 }
 
--- serveresp/serveresp.ino
+++ serveresp/serveresp.ino
@@ -1,14 +1,24 @@
+#include <IRremote.hpp>
 #include <LoRa.h>
+#include <SPI.h>
 
-
+//LoRa Pins
+#define ss 5
+#define rst 14
+#define dio0 2
 
 void setup() {
   // initilize serial
-  Serial.begin(9600);
+  Serial.begin(115200);
+  // initilize LoRa and IR
+  LoRa.setPins(ss, rst, dio0);
+  LoRa.begin(866E6);
+  LoRa.setSyncWord(0xF3);
+  
+  
 }
 
 void loop() {
 
-  Serial.println(analogRead(A1));
-  delay(1);
+
 }
